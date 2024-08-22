d4034b56cfb207f8d616ca3d2f39fd6ebf3863c5
xnenadp <pn.petkovic.nenad@gmail.com>
2024-08-22 19:56:37 +0200

    Filters fix

--- spaceships/spaceships/src/components/Filters/index.tsx
+++ spaceships/spaceships/src/components/Filters/index.tsx
@@ -4,12 +4,13 @@ import * as Styled from "./Filters.styled";
 import PulseLaserFilter from "./PulseLaserFilter";
 import { FC } from "react";
 import AverageSpeedFilter from "./AverageSpeedFilter";
+import { FilterType } from "@/utils/constants";
 
 interface Props {
   filters: FiltersType;
   setPulseLaser: (value?: boolean) => void;
   setColors: (colors: string[]) => void;
-  setAverageSpeed: (filter: { type: string; value: [number, number] }) => void;
+  setAverageSpeed: (filter: { type: FilterType; minValue: number; maxValue: number }) => void;
 }
 
 const Filters: FC<Props> = ({ filters, setPulseLaser, setColors, setAverageSpeed}) => {
